39a79e1d14c59515bf629e8bdfb9e349800b500e
ans92 <51845963+ans92@users.noreply.github.com>
2024-07-17 19:39:17 +0500

    Update flags.py

--- flags.py
+++ flags.py
@@ -1,6 +1,6 @@
 import argparse
 
-DATA_FOLDER ="/home/ans/DATA_ROOT/"
+DATA_FOLDER ="DATA_ROOT/"
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser.add_argument('--config', default='configs/args.yml', help='path of the config file (training only)')
